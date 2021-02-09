502afe1e3529ec7fd5364ada546cd1242524bd3c
KevinCouillard <kevincouillard48@gmail.com>
2021-02-09 17:34:09 -0500

    Demo 3 (Messenger): Kevin Couillard

---
+++ app/src/main/java/com/hfad/messenger/CreateMessegeActivity.java
@@ -0,0 +1,29 @@
+package com.hfad.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessegeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_messege);
+    }
+
+    //Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        startActivity(chosenIntent);
+
+    }
+ }
